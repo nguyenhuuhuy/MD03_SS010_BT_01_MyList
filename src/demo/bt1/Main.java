38748fd5017f9e39ccb4af45852e1a0118f90400
nguyenhuuhuy <huy.cb96@gmail.com>
2023-03-16 23:26:18 +0700

    bt

--- src/demo/bt1/Main.java
+++ src/demo/bt1/Main.java
@@ -1,13 +1,27 @@
 package demo.bt1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        MyList<Integer> myList = new MyList<>();
-        myList.add(1);
-        myList.ensureCapacity(2);
-        System.out.println(myList.get(2));
-        myList.clear();
+        MyList<Integer> myList = new MyList<>(5);
+        myList.add(0,15);
+        myList.add(55);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4); // vị trí thứ 4
+        myList.add(5);
+        System.out.println("mảng ban đầu");
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.print(myList.get(i)+ " ");
+        }
+        System.out.println("\nĐộ dài của mảng hiện tại: "+myList.size);
+        // xóa ở vị trí thứ 4
+        myList.remove(4);
+        System.out.println("sau khi xóa bỏ 1 phần tử độ dài mảng thay đổi: "+myList.size);
+        // kiểm tra xem có phần tử ở trong mảng không true/false
+        System.out.println("kiểm tra sự tồn tại của phần tử trong mảng: "+myList.contains(4));
+        System.out.println("vị trí: " +myList.indexOf(55) );
     }
 }
--- src/demo/bt1/MyList.java
+++ src/demo/bt1/MyList.java
@@ -1,53 +1,116 @@
 package demo.bt1;
-
-public class MyList<E> {
+import java.util.ArrayList;
+import java.util.Arrays;
+public class MyList<E>  {
     int size = 0;
     static final int DEFAULT_CAPACITY = 10;
     Object elements[];
-
+    // kích thước mặc định khi được khởi tạo
     public MyList() {
         elements = new Object[DEFAULT_CAPACITY];
     }
-
+    // tạo mảng có kích thước định trc
     public MyList(int capacity) {
-
+        this.elements = new Object[capacity];
     }
-    public void add(int index) {
-//        System.out.println(index);
+    // thêm phần tử vào vị trí trong mảng
+    public void add(int index,E element) {
+        //Kiểm tra tính hợp lệ của giá trị index truyền vào, thông báo lỗi nếu sai,
+        if (index < 0 || index > size) {
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            //Mở rộng mảng dữ liệu hiện tại thành mảng mới, nếu cần thiết
+            //Dùng biến "temp" để lưu trữ tạm thời, đồng thời gán dữ liệu vào phần từ thứ index của mảng
+            //Duyệt mảng từ phần tử index + 1(do index đã gán dữ liệu rồi) và tiến hành hoán đổi biến "temp" với các vị trí kế tiếp
+            //đồng thời tăng kích thước mảng thực tế 1 đơn vị
+            ensureCapacity();
+            Object temp = elements[index];
+            elements[index] = element;
+            for (int i = index + 1; i < size + 1; i++) {
+                elements[i] = temp;
+                temp = elements[i + 1];
+            }
+            size++;
+        }
     }
-    public E remove(int index){
-
-        return null;
+    E remove(int index){
+        //Kiểm tra tính hợp lệ của giá trị index truyền vào, thông báo lỗi nếu sai,
+        //nếu index hợp lệ thì duyệt mảng từ vị trí index, dùng hoán đổi để dịch dữ liệu qua phải
+        //giảm giá trị mảng thực tế 1 đơn vị
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException("Index: " + index + ", Actual size: " + size);
+        } else {
+            Object deletedElements = elements[index];
+            for (int i = index; i < size; i++) {
+                Object temp = elements[i];
+                elements[i] = elements[i + 1];
+                elements[i + 1] = temp;
+            }
+//            elements[size - 1] = null;
+            size--;
+            return (E) deletedElements;
+        }
     }
-
+    // kích thước mảng
     public int size(){
-
-        return 0;
+        return this.size;
     }
+    // sao chép mảng
     public E clone(){
-
-        return null;
+        Object cloneArray = Arrays.copyOf(this.elements, size);
+        return (E) cloneArray;
     }
+    // kiểm tra sự tồn tại phần tử trong mảng trả về true/false
     public boolean contains(E o) {
 
-        return true;
+        return indexOf(o)>0;
     }
+    // tìm vị trí phần tử trong mảng
     public int indexOf(E o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elements[i] == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elements[i])) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+     boolean add(E o) {
+         //Mở rộng mảng dữ liệu hiện tại thành mảng mới, nếu cần thiết
+         //Gán dữ liệu vào phần từ cuối của mảng
+         //đồng thời tăng kích thước mảng thực tế 1 đơn vị
+         ensureCapacity();
+         elements[size++] = o;
+         return true;
+    }
+    // mở rộng mảng dữ liệu
+    private void ensureCapacity(){
+        if (size == elements.length) {
+            //Nhân đôi kích thước mảng khai báo và copy dữ liệu sang mảng mới
+            int newSize = elements.length * 2;
+            this.elements = (Object[]) Arrays.copyOf(elements, newSize);
+        }
+    }
+    // trả ra phần tử
+    public E get(int index){
 
-        return 0;
+        //Kiểm tra tính hợp lệ của giá trị index truyền vào, thông báo lỗi nếu sai,
+        // trả ra kết quả nếu giá trị hợp lệ
+        if (index < 0 || index >= size) {
+            throw new ArrayIndexOutOfBoundsException("Index: " + index + ", Actual size: " + size);
+        } else {
+            return (E) elements[index];
+        }
     }
-    public boolean add(E o) {
-
-        return true;
-    }
-    public void ensureCapacity(int minCapacity){
-        System.out.println("ok");
-    }
-    public E get(int i){
-
-        return null;
-    }
+    // xóa all các phần tử trong mảng kích thước trở về 0
     public void clear(){
-        System.out.println("xong");
+       this.size = 0;
     }
 }
